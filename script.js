d77878aff0884e3e820a61ce66ad13c816d3f313
JASPREET KAUR <94101161+jasbhatia@users.noreply.github.com>
2023-03-19 17:19:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 // complete the given function
 
 function palindrome(str){
-var res= str.toLowerCase();
-	let start=0;
-	let end=res.length;
-	while(start<end){
-		if(res.charAt(start)!==res.charAt(end){
-			return false;
-		}
-		else{
-			start++;
-			end--;
-		}
-	}
-	return true;
-	
+
+ const string = str.toLowerCase();
+ const len = str.length;
+
+    // loop through half of the string
+    for (let i = 0; i < len / 2; i++) {
+
+        // check if first and last string are same
+        if (string[i] !== string[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 }
 module.exports = palindrome
